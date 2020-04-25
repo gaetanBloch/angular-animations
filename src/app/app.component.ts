8b91b8d60812de08889af1cb5c50610af6df86ea
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-25 11:45:15 +0200

    Using the void state in the element list animations add and remove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,6 +43,25 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
         })),
         animate(500)
       ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+        ]),
+      transition('* => void', [
+        animate(300, style({
+          opacity: 0,
+          transform: 'translateX(100px)'
+        }))
+      ])
     ])
   ]
 })
